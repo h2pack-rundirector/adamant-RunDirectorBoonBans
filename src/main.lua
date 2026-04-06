532c33c81623a2f08e43c64d1d708cd3c30ce842
maybe-adamant <maybe.adamant@gmail.com>
2026-04-06 10:47:41 -0400

    fixing default values

--- src/main.lua
+++ src/main.lua
@@ -10,6 +10,7 @@ local chalk = mods["SGG_Modding-Chalk"]
 local reload = mods["SGG_Modding-ReLoad"]
 lib = mods["adamant-ModpackLib"]
 
+local dataDefaults = import("config.lua")
 local config = chalk.auto("config.lua")
 
 local PACK_ID = "run-director"
@@ -31,13 +32,13 @@ public.definition = {
 internal.definition = public.definition
 
 public.definition.storage = {
-    { type = "bool",   alias = "EnablePadding",                   configKey = "EnablePadding",                   default = config.EnablePadding },
-    { type = "int",    alias = "Padding_PrioritizeCoreForFirstN", configKey = "Padding_PrioritizeCoreForFirstN", default = config.Padding_PrioritizeCoreForFirstN, min = 0, max = 15 },
-    { type = "bool",   alias = "Padding_AvoidFutureAllowed",      configKey = "Padding_AvoidFutureAllowed",      default = config.Padding_AvoidFutureAllowed },
-    { type = "bool",   alias = "Padding_AllowDuos",               configKey = "Padding_AllowDuos",               default = config.Padding_AllowDuos },
-    { type = "int",    alias = "ImproveFirstNBoonRarity",         configKey = "ImproveFirstNBoonRarity",         default = config.ImproveFirstNBoonRarity, min = 0, max = 15 },
-    { type = "string", alias = "BridalGlowTargetBoon",            configKey = "BridalGlowTargetBoon",            default = config.BridalGlowTargetBoon, maxLen = 128 },
-    { type = "int",    alias = "ViewRegion",                      configKey = "ViewRegion",                      default = config.ViewRegion },
+    { type = "bool",   alias = "EnablePadding",                   configKey = "EnablePadding" },
+    { type = "int",    alias = "Padding_PrioritizeCoreForFirstN", configKey = "Padding_PrioritizeCoreForFirstN", min = 0, max = 15 },
+    { type = "bool",   alias = "Padding_AvoidFutureAllowed",      configKey = "Padding_AvoidFutureAllowed" },
+    { type = "bool",   alias = "Padding_AllowDuos",               configKey = "Padding_AllowDuos" },
+    { type = "int",    alias = "ImproveFirstNBoonRarity",         configKey = "ImproveFirstNBoonRarity",         min = 0, max = 15 },
+    { type = "string", alias = "BridalGlowTargetBoon",            configKey = "BridalGlowTargetBoon",            maxLen = 128 },
+    { type = "int",    alias = "ViewRegion",                      configKey = "ViewRegion" },
 }
 public.definition.ui = {}
 
@@ -56,12 +57,11 @@ do
             type = "int",
             alias = item.key,
             configKey = item.key,
-            default = item.default,
         })
     end
 end
 
-public.store = lib.createStore(config, public.definition)
+public.store = lib.createStore(config, public.definition, dataDefaults)
 store = public.store
 
 local function SyncPublicExports()
